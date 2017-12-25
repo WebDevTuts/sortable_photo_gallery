9be6343062ce1536b4160e820d283297c43ba97c
Edwin Chen <edwinchen85@gmail.com>
2017-12-25 09:58:31 +0800

    Prepare initial settings

--- includes/sortable_gallery.js
+++ includes/sortable_gallery.js
@@ -1,4 +1,9 @@
+var thumbnailSpacing = 15;
+
 $(document).ready(function() {
+  $('.gallery .sorting').css('margin-bottom', window.thumbnailSpacing+'px');
+  $('.thumbnail_container a.thumbnail').addClass('showMe');
+
   positionThumbnails();
 });
 
